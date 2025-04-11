cd852f0e7ed5cacc0b1d76c8062e4bfa60ccaa1b
Inga Kononova <inga.kononova@gmail.com>
2025-04-11 20:02:33 +0300

    Created addTask

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.2.5.
 
-## Development server
+## My comments about app
+Single page application, without SSR
 Zoneless
 
 ## Development server
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, model, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface Task {
+  id: number,
+  text: string,
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo-app-angular19';
+  tasks = signal<Task[]>([]);
+  taskText = model('');
+
+  addTask() {
+    this.tasks.update(tasks => {
+      const taskId = tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1;
+
+      tasks.push({
+        id: taskId,
+        text: this.taskText()
+      });
+      return tasks
+    })
+    this.taskText.set('')
+  }
+
 }
