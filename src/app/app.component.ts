d681082f2c933c42b14828ea8381b73eb324cd57
Inga Kononova <inga.kononova@gmail.com>
2025-04-13 21:53:42 +0300

    Created Edit functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,9 @@ export class AppComponent {
   tasks = signal<Task[]>([]);
   taskText = model('');
 
+  editIndex = signal<number | null>(null);
+  editText = model('');
+
   addTask() {
     this.tasks.update(tasks => {
       const taskId = tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1;
@@ -34,6 +37,27 @@ export class AppComponent {
       tasks.splice(index, 1)
       return tasks
     })
+  }
+
+  editTask(index: number) {
+    const task = this.tasks()[index];
+    this.editIndex.set(index)
+    this.editText.set(task.text);
+  }
+
+  saveTask(){
+    const index = this.editIndex();
+    if (index === null) return;
+
+    this.tasks.update(tasks => {
+      const updated = [...tasks];
+      updated[index] = { ...updated[index], text: this.editText()
+      };
+    return updated;
+    });
+
+    this.editIndex.set(null);
+    this.editText.set('');
   }
 
 }
